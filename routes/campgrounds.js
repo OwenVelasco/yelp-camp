bf3214840348bf8ee723588c9ac3cd03522e7fbc
Owen <owenvelasco1997@gmail.com>
2019-11-11 12:10:22 -0500

    add pagination to index page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,14 +18,32 @@ var geocoder = NodeGeocoder(options);
 
 
 //INDEX - show all campgrounds
+// router.get("/", function (req, res) {
+//     // Get all campgrounds from DB
+//     Campground.find({}, function (err, allCampgrounds) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.render("campgrounds/index", {campgrounds: allCampgrounds});
+//         }
+//     });
+// });
 router.get("/", function (req, res) {
-    // Get all campgrounds from DB
-    Campground.find({}, function (err, allCampgrounds) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
-        }
+    var perPage = 8;
+    var pageQuery = parseInt(req.query.page);
+    var pageNumber = pageQuery ? pageQuery : 1;
+    Campground.find({}).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, allCampgrounds) {
+        Campground.countDocuments().exec(function (err, count) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", {
+                    campgrounds: allCampgrounds,
+                    current: pageNumber,
+                    pages: Math.ceil(count / perPage)
+                });
+            }
+        });
     });
 });
 
